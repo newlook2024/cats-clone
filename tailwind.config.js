d44dd482fd617178e43e90e3c10146c64783a610
newlook2024 <abdulvahob2022@gmail.com>
2024-06-03 12:58:48 +0500

    background colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black1: "#1A212F",
+      },
+      fontSize: {
+        3.5: "14px",
+      },
+      margin: {
+        17.5: "70px",
+      },
+      backgroundColor: {
+        orange: "#FAC663",
+      },
+      padding: {
+        17.5: "70px",
+      },
+    },
   },
   plugins: [],
 };
